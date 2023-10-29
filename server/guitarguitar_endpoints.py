425ba3b22721af025de521f8a9c1e7121ae1ae38
andrew-holligan <andyholl2014@outlook.com>
2023-10-29 10:30:36 +0000

    feat: setup order and product endpoints

--- server/guitarguitar_endpoints.py
+++ server/guitarguitar_endpoints.py
@@ -1,6 +1,8 @@
 import requests
 
 customers = None
+orders = None
+products = None
 
 
 def get_customers(force_fetch=False):
@@ -15,3 +17,31 @@ def get_customers(force_fetch=False):
     customers = r.json()
 
     return customers
+
+
+def get_orders(force_fetch=False):
+    global orders
+
+    if orders and not force_fetch:
+        return orders
+
+    # Prepare a custom response
+    url = "https://www.guitarguitar.co.uk/hackathon/orders/"
+    r = requests.get(url=url)
+    orders = r.json()
+
+    return orders
+
+
+def get_products(force_fetch=False):
+    global products
+
+    if products and not force_fetch:
+        return products
+
+    # Prepare a custom response
+    url = "https://www.guitarguitar.co.uk/hackathon/products/"
+    r = requests.get(url=url)
+    products = r.json()
+
+    return products
